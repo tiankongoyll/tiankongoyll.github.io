3d9bf4111a8a248bc373c6155502d777e559bed8
iMac <iMac@iMac.local>
2017-10-23 17:21:50 +0800

    sdsd

--- weixin/js/aaa.js
+++ weixin/js/aaa.js
@@ -1,12 +1,40 @@
-var pageHeight = window.innerHeight;
-alert(document.getElementById("mmain").clientHeight * document.documentElement.clientWidth / 750;)
-mui("#bodym").on('swipeup', '.mmain', function() {
-//				alert("changans上画")
-			$(".mmain").animate({ 
-			   marginTop:parseInt($(".mmain").css("marginTop"))-pageHeight
-//				marginTop:"-300"
-			  }, 1000 )
-//alert("ss")
-
-alert(parseInt($(".mmain").css("marginTop")))
-			})
+//window.onload = window.onresize = function() {
+	
+	//alert(document.getElementById("mmain").clientHeight * document.documentElement.clientWidth / 750)
+	mui("#bodym").on('swipeup', '.mmain', function() {
+		var pageHeight = window.innerHeight;
+		var nam=document.getElementById("mmain").clientHeight * document.documentElement.clientWidth / 750
+		var nam1 = parseInt(nam / pageHeight);
+		console.log(nam1)
+		
+		if(parseInt($(".mmain").css("marginTop"))<(nam1-1)*pageHeight-50) {
+			console.log("上移")
+			$(".mmain").animate({
+				marginTop: parseInt($(".mmain").css("marginTop")) - pageHeight
+			}, 1000)
+		}else{
+			console.log("上移置顶")
+			$(".mmain").animate({
+				marginTop: nam-nam1*pageHeight
+			}, 1000)
+		}
+	})
+	mui("#bodym").on('swipedown', '.mmain', function() {
+		var pageHeight = window.innerHeight;
+		var nam=document.getElementById("mmain").clientHeight * document.documentElement.clientWidth / 750
+		var nam1 = parseInt(nam / pageHeight);
+		console.log(nam1)
+		
+		if(parseInt($(".mmain").css("marginTop"))>pageHeight) {
+			console.log("下滑")
+			$(".mmain").animate({
+				marginTop: $(".mmain").css("marginTop") + pageHeight
+			}, 1000)
+		}else{
+			console.log("下滑置顶")
+			$(".mmain").animate({
+				marginTop: 0
+			}, 1000)
+		}
+	})
+//}
